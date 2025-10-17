f410f6fbb3abefa337cdd6b7c71a0535bd2a3f01
sauga77kjk <169109325+sauga77kjk@users.noreply.github.com>
2025-10-17 18:39:11 +0900

    Update hey guys.lua

--- hey guys.lua
+++ hey guys.lua
@@ -1,1 +1,6 @@
-print('how u doing bro')
+local character = owner.Character
+for _,v in next,character:GetDescendants() do
+  if v:IsA("BasePart") then
+    v.CustomPhysicalProperties = PhysicalProperties.new(100,0.3,0.5,1,1)
+  end
+end
