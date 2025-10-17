0de00aa36c7a4ed83c5a15dbf248d009c6b5b0fe
sauga77kjk <169109325+sauga77kjk@users.noreply.github.com>
2025-10-17 19:27:32 +0900

    Update hey 2.lua

--- hey 2.lua
+++ hey 2.lua
@@ -1,15 +1,8 @@
-local limbsc1 = {
-	--left
-	lh = CFrame.new(3.5, 1, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0),
-	ls = CFrame.new(4.5, 0.5, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0),
-	--right
-	rh = CFrame.new(-3.5, 1, 0, 0, 0, 1, 0, 1, 0, -1, 0, 0),
-	rs = CFrame.new(-4.5, 0.5, 0, 0, 0, 1, 0, 1, 0, -1, 0, 0)
-}
 local torsosize = Vector3.new(10, 2, 1)
 local chr = owner.Character
 chr.Torso.Size = torsosize
-chr.Torso["Left Shoulder"].C1 = limbsc1.ls
-chr.Torso["Left Hip"].C1 = limbsc1.lh
-chr.Torso["Right Hip"].C1 = limbsc1.rh
-chr.Torso["Right Shoulder"].C1 = limbsc1.rs
+chr.Torso["Left Hip"].C1 = CFrame.new(3.5, 1, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+chr.Torso["Left Shoulder"].C1 = CFrame.new(4.5, 0.5, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+chr.Torso["Right Hip"].C1 = CFrame.new(-3.5, 1, 0, 0, 0, 1, 0, 1, 0, -1, 0, 0)
+chr.Torso["Right Shoulder"].C1 = CFrame.new(-4.5, 0.5, 0, 0, 0, 1, 0, 1, 0, -1, 0, 0)
+warn'hi bros'
